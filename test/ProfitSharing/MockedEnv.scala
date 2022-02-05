ef7597e771207efb6d1cf329a30c083aaadcd25e
fateme.r <fatemeh.r3366@gmail.com>
2022-02-05 17:34:19 +0330

    Testing reorganizing and better doc strings

---
+++ test/ProfitSharing/MockedEnv.scala
@@ -0,0 +1,76 @@
+package ProfitSharing
+import helpers.Configs
+import network.Client
+import org.ergoplatform.appkit.{BlockchainContext, CoveringBoxes, ErgoToken, InputBox}
+import org.ergoplatform.appkit.impl.ErgoTreeContract
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito._
+
+import scala.collection.immutable.List
+import collection.JavaConverters._
+
+class MockedEnv (client: Client, contracts: Contracts) {
+  private val mockedClient = mock(classOf[Client])
+  private val mockedCtx: BlockchainContext = mock(classOf[BlockchainContext])
+  val tokenId1 = "00ee077854471a04fbef18a5a971b50fb39f52fc6f6b3b8d0682ce2c48f6ebef"
+  val tokenId2 = "11ee077854471a04fbef18a5a971b50fb39f52fc6f6b3b8d0682ce2c48f6ebef"
+
+  def getMockedClient: Client = mockedClient
+  def getMockedCtx: BlockchainContext = mockedCtx
+
+  def randomId(): String ={
+    val randomBytes = Array.fill(32)((scala.util.Random.nextInt(256) - 128).toByte)
+    randomBytes.map("%02x" format _).mkString
+  }
+
+  when(mockedClient.getAllUnspentBox(contracts.incomeAddress)).thenReturn({
+    var boxList: List[InputBox] = List()
+    client.getClient.execute({ctx =>{
+      for(i <- 1 to Configs.incomeMerge.min) {
+        val txB = ctx.newTxBuilder()
+        boxList = boxList :+
+          txB.outBoxBuilder()
+            .value(1e9.toLong)
+            .contract(contracts.income)
+            .build().convertToInputWith(randomId(), 1)
+      }
+      for(i <- 1 until Configs.incomeMerge.min) {
+        val txB = ctx.newTxBuilder()
+        boxList = boxList :+
+          txB.outBoxBuilder()
+            .value((1e9*0.001).toLong)
+            .contract(contracts.income)
+            .tokens(new ErgoToken(tokenId1, 10))
+            .build().convertToInputWith(randomId(), 1)
+      }
+      for(i <- 1 to Configs.incomeMerge.max) {
+        val txB = ctx.newTxBuilder()
+        boxList = boxList :+
+          txB.outBoxBuilder()
+            .value((1e9*0.001).toLong)
+            .contract(contracts.income)
+            .tokens(new ErgoToken(tokenId2, 10))
+            .build().convertToInputWith(randomId(), 1)
+      }
+    }})
+    boxList
+  })
+  when(mockedClient.getCoveringBoxesFor(Configs.initializer.address, Configs.fee*8)).thenReturn({
+    client.getClient.execute(ctx =>{
+      val txB = ctx.newTxBuilder()
+      val box = txB.outBoxBuilder()
+        .value(1e9.toLong)
+        .contract(new ErgoTreeContract(Configs.initializer.address.getErgoAddress.script))
+        .build().convertToInputWith(randomId(), 1)
+      new CoveringBoxes(Configs.fee * 8, List(box).asJava)
+    })
+  })
+
+  when(mockedCtx.newTxBuilder()).thenAnswer(_ => {
+    client.getClient.execute(_.newTxBuilder())
+  })
+  when(mockedCtx.newProverBuilder()).thenAnswer(_ => {
+    client.getClient.execute(_.newProverBuilder())
+  })
+  when(mockedCtx.sendTransaction(any())).thenReturn(randomId())
+}
--- test/ProfitSharing/ProfitSharingSpec.scala
+++ test/ProfitSharing/ProfitSharingSpec.scala
@@ -5,12 +5,11 @@ import helpers.{Configs, Utils}
 import org.scalatest.propspec._
 import network.Client
 import org.ergoplatform.appkit.impl.ErgoTreeContract
-import org.ergoplatform.appkit.{BlockchainContext, CoveringBoxes, ErgoToken, InputBox, SignedTransaction}
+import org.ergoplatform.appkit.{ErgoToken, InputBox}
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito._
 import org.scalatest.matchers.should
 
-import collection.JavaConverters._
 import scala.collection.immutable._
 
 class ProfitSharingSpec extends AnyPropSpec with should.Matchers{
@@ -19,97 +18,53 @@ class ProfitSharingSpec extends AnyPropSpec with should.Matchers{
   client.setClient()
   val contracts = new Contracts(client, utils)
 
-  val tokenId1 = "00ee077854471a04fbef18a5a971b50fb39f52fc6f6b3b8d0682ce2c48f6ebef"
-  val tokenId2 = "11ee077854471a04fbef18a5a971b50fb39f52fc6f6b3b8d0682ce2c48f6ebef"
-  val mockedClient: Client = mock(classOf[Client])
-  when(mockedClient.getAllUnspentBox(contracts.incomeAddress)).thenReturn({
-    var boxList: List[InputBox] = List()
-    client.getClient.execute({ctx =>{
-      for(i <- 1 to Configs.incomeMerge.min) {
-        val txB = ctx.newTxBuilder()
-        boxList = boxList :+
-          txB.outBoxBuilder()
-            .value(1e9.toLong)
-            .contract(contracts.income)
-            .build().convertToInputWith(randomId(), 1)
-      }
-      for(i <- 1 until Configs.incomeMerge.min) {
-        val txB = ctx.newTxBuilder()
-        boxList = boxList :+
-          txB.outBoxBuilder()
-            .value((1e9*0.001).toLong)
-            .contract(contracts.income)
-            .tokens(new ErgoToken(tokenId1, 10))
-            .build().convertToInputWith(randomId(), 1)
-      }
-      for(i <- 1 to Configs.incomeMerge.max) {
-        val txB = ctx.newTxBuilder()
-        boxList = boxList :+
-          txB.outBoxBuilder()
-            .value((1e9*0.001).toLong)
-            .contract(contracts.income)
-            .tokens(new ErgoToken(tokenId2, 10))
-            .build().convertToInputWith(randomId(), 1)
-      }
-    }})
-    boxList
-  })
-  when(mockedClient.getCoveringBoxesFor(Configs.initializer.address, Configs.fee*8)).thenReturn({
-    client.getClient.execute(ctx =>{
-      val txB = ctx.newTxBuilder()
-      val box = txB.outBoxBuilder()
-        .value(1e9.toLong)
-        .contract(new ErgoTreeContract(Configs.initializer.address.getErgoAddress.script))
-        .build().convertToInputWith(randomId(), 1)
-      new CoveringBoxes(Configs.fee * 8, List(box).asJava)
-    })
-  })
-
-  val mockedCtx: BlockchainContext = mock(classOf[BlockchainContext])
-  when(mockedCtx.newTxBuilder()).thenAnswer(_ => {
-    client.getClient.execute(_.newTxBuilder())
-  })
-  when(mockedCtx.newProverBuilder()).thenAnswer(_ => {
-    client.getClient.execute(_.newProverBuilder())
-  })
-  when(mockedCtx.sendTransaction(any())).thenReturn(randomId())
-
-//  val fakeTx: SignedTransaction = createProcedureObject.tokenIssueTx(mockedCtx, 10, mockedClient.getCoveringBoxesFor(Configs.initializer.address, Configs.fee*8), Configs.initializer.address, "test token", "test token")
-
-  def createBoxObject: Boxes = new Boxes(mockedClient, utils, contracts)
-  def createProcedureObject: Procedures = new Procedures(mockedClient, createBoxObject, contracts, utils)
-
-  def randomId(): String ={
-    val randomBytes = Array.fill(32)((scala.util.Random.nextInt(256) - 128).toByte)
-    randomBytes.map("%02x" format _).mkString
+  def getMockedBoxes(mockedEnv: MockedEnv): Boxes = new Boxes(mockedEnv.getMockedClient, utils, contracts)
+  def getMockedProcedure(mockedEnv: MockedEnv): Procedures = {
+    val boxes = new Boxes(mockedEnv.getMockedClient, utils, contracts)
+    new Procedures(mockedEnv.getMockedClient, boxes, contracts, utils)
   }
 
   /**
-   * Testing the getIncomes
-   * It should return the aggregated list of boxes that are ready to be merged
-   * The output must have two lists one for Erg and one for the required token type
+   * Target: testing getIncomes
+   * Dependencies:
+   *    Client & Ctx
+   * Procedure:
+   *    1- mocking environment
+   *    2- Calling getIncome function
+   * Expected Output:
+   *    The function should return a list of incomes which are ready to merge
+   *    The list should have 2 inner lists
+   *    Each list should contain [mergeIncome.min, mergeIncome.max) boxes, The first one testing the lower bound and second testing the upper bound.
    */
-  property("Testing income box merging") {
-    val boxes = createBoxObject
+  property("Testing income box search") {
+    val mockedEnv = new MockedEnv(client, contracts)
+    val boxes = getMockedBoxes(mockedEnv)
     val result = boxes.getIncomes
     result.size should be (2)
     result(0).size should be (Configs.incomeMerge.min)
     result(1).size should be (Configs.incomeMerge.max)
-    result(1).head.getTokens.get(0).getId.toString should be (tokenId2)
+    result(1).head.getTokens.get(0).getId.toString should be (mockedEnv.tokenId2)
   }
 
   /**
-   * Testing the tokenIssueTx
-   * It should return a transaction that issued a new token with specified details
-   * The output must have a new token type with the specified amount
+   * Target: testing tokenIssue
+   * Dependencies:
+   *    Ctx
+   * Procedure:
+   *    1- mocking environment
+   *    2- mocking inputBox
+   *    2- Calling tokenIssue function
+   * Expected Output:
+   *    The function should return a transaction issued a new token with required amount
    */
   property("Testing token issue tx") {
-    val procedures = createProcedureObject
-    val mockedInputBox = mockedCtx.newTxBuilder().outBoxBuilder()
+    val mockedEnv = new MockedEnv(client, contracts)
+    val procedures = getMockedProcedure(mockedEnv)
+    val mockedInputBox = mockedEnv.getMockedCtx.newTxBuilder().outBoxBuilder()
       .value(1e9.toLong)
       .contract(new ErgoTreeContract(Configs.initializer.address.getErgoAddress.script))
-      .build().convertToInputWith(randomId(), 1)
-    val tx = procedures.tokenIssueTx(mockedCtx, 10, List(mockedInputBox), Configs.initializer.address, "test token", "test token")
+      .build().convertToInputWith(mockedEnv.randomId(), 1)
+    val tx = procedures.tokenIssueTx(mockedEnv.getMockedCtx, 10, List(mockedInputBox), Configs.initializer.address, "test token", "test token")
     tx.getOutputsToSpend.size() should be (3)
     tx.getOutputsToSpend.get(1).getTokens.size() should be (1)
     tx.getOutputsToSpend.get(1).getValue should be (Configs.fee)
@@ -118,22 +73,29 @@ class ProfitSharingSpec extends AnyPropSpec with should.Matchers{
   }
 
   /**
-   * Testing mergeIncomesTx
-   * It should return a transaction that merged the required boxes into one
-   * The merged box must contain all gathered tokens and erg
+   * Target: testing mergeIncomeTx
+   * Dependencies:
+   *    Ctx
+   * Procedure:
+   *    1- mocking environment
+   *    2- mocking inputBoxes
+   *    2- Calling mergeIncome function
+   * Expected Output:
+   *    The function should return a transaction merging all input incomes
    */
   property("Testing merge income tx") {
-    val procedures = createProcedureObject
+    val mockedEnv = new MockedEnv(client, contracts)
+    val procedures = getMockedProcedure(mockedEnv)
     var inputList: List[InputBox] = List()
-    val txB = mockedCtx.newTxBuilder()
+    val txB = mockedEnv.getMockedCtx.newTxBuilder()
     for(i <- 1 to Configs.incomeMerge.min) {
       inputList = inputList :+ txB.outBoxBuilder()
         .value((1e9 * 0.1).toLong)
         .contract(contracts.income)
-        .tokens(new ErgoToken(tokenId1, 10))
-        .build().convertToInputWith(randomId(), 1)
+        .tokens(new ErgoToken(mockedEnv.tokenId1, 10))
+        .build().convertToInputWith(mockedEnv.randomId(), 1)
     }
-    val tx = procedures.mergeIncomesTx(inputList, mockedCtx)
+    val tx = procedures.mergeIncomesTx(inputList, mockedEnv.getMockedCtx)
     tx.getOutputsToSpend.size() should be (2)
     tx.getOutputsToSpend.get(0).getTokens.size() should be (1)
     tx.getOutputsToSpend.get(0).getValue.toLong should be >= ((1e9 * 0.1)*Configs.incomeMerge.min - Configs.incomeMerge.maxFee).toLong
@@ -142,28 +104,50 @@ class ProfitSharingSpec extends AnyPropSpec with should.Matchers{
   }
 
   /**
-   * Testing serviceInitialization
-   * It should create the service box and return a list of 3 elements denoting the tokens
-   * The service box must have three token types and the correct contract
+   * Target: testing serviceInitialization
+   * Dependencies:
+   *    Ctx
+   *    Procedures.tokenIssueTx
+   * Procedure:
+   *    1- mocking environment
+   *    2- Calling serviceInitialization function
+   * Expected Output:
+   *    The function should create the service box in 4 transactions
+   *    The function should return 3 token ids related to service
    */
   property("Testing service initialization") {
-    val procedures = createProcedureObject
-    val result = procedures.serviceInitialization(mockedCtx)
+    val mockedEnv = new MockedEnv(client, contracts)
+    val procedures = getMockedProcedure(mockedEnv)
+    val result = procedures.serviceInitialization(mockedEnv.getMockedCtx)
     result should have size 3
+    // TODO: Mock the tokenIssueTx and change this to 1 time
+    verify(mockedEnv.getMockedCtx, times(4)).sendTransaction(any())
   }
 
   /**
-   * Testing mergeIncome
-   * It should call mergeIncomeTx for each set of income boxes
+   * Target: testing mergeIncome
+   * Dependencies:
+   *    Client & Ctx
+   *    Boxes
+   *    Procedures.mergeIncomeTx
+   * Procedure:
+   *    1- mocking environment
+   *    1- mocking boxes object (getIncomes)
+   *    2- mocking Procedure.mergeIncomeTx
+   *    3- Calling mergeIncome function
+   * Expected Output:
+   *    The function should get incomes once
+   *    The function should call mergeIncomeTx 2 times (2 input lists were passed)
    */
   property("Testing merge income service") {
-    val boxes: Boxes = createBoxObject
+    val mockedEnv = new MockedEnv(client, contracts)
+    val boxes = getMockedBoxes(mockedEnv)
     val spyBoxes: Boxes = spy(boxes)
     doReturn(List(List(), List()), List(List(), List())).when(spyBoxes).getIncomes
-    val procedures = new Procedures(mockedClient, spyBoxes, contracts, utils)
+    val procedures = new Procedures(mockedEnv.getMockedClient, spyBoxes, contracts, utils)
     val spyProcedures = spy(procedures)
-    doReturn(null, null).when(spyProcedures).mergeIncomesTx(List(), mockedCtx)
-    spyProcedures.mergeIncomes(mockedCtx)
+    doReturn(null, null).when(spyProcedures).mergeIncomesTx(List(), mockedEnv.getMockedCtx)
+    spyProcedures.mergeIncomes(mockedEnv.getMockedCtx)
     verify(spyBoxes, times(1)).getIncomes
     verify(spyProcedures, times(2)).mergeIncomesTx(any(), any())
   }
